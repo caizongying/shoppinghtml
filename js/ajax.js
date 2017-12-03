4c572d9b385f836b008d6fcb1f7c213693178044
asus <1085484597@qq.com>
2017-12-03 09:55:54 +0800

    dad

--- js/ajax.js
+++ js/ajax.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	$("#footer-newsletter-button").click(function() {
 		var a=$.ajax({
-			type: "POST",
-			data: "email="+escape($('#footer-newsletter-input').val()),
+            type: 'POST',
+            data: "email="+$('#footer-newsletter-input').val(),
+            dataType: 'json',
 			url: "http://localhost:8090/email",
 			success: function() {
 				alert("订购成功");
@@ -11,6 +12,26 @@ $(document).ready(function() {
 				alert("订购失败");
 			}
 		})
-        alert(window.location.href);
+		alert("234");
 	});
+
+	$("#login-submit").click(function () {
+		var params={
+		      username:$("#billing_username").val(),
+			  password:$("#billing_password").val()
+		};
+		var b=$.ajax({
+            type: 'POST',
+			  data:params,
+			  url:"http://localhost:8090/login",
+			  dataType:'json',
+			success:function(data){
+            alert("登录成功");
+		},
+		      error:function(data){
+                  alert("登录失败");
+			  }});
+    });
+
+
 });

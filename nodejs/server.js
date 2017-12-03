2c9f634599ca0bf24f0000ff151499411dfe8415
asus <1085484597@qq.com>
2017-12-03 09:51:15 +0800

    dad

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,31 +1,39 @@
-var http=require("http");
 var express=require("express");
 var app=express();
 var nodemailer = require("nodemailer");
+var mysql = require('mysql');
 
 
+//连接数据库
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'test'
+});  
+connection.connect();
 
-
-app.post("/email",function(req,res,next){ 
-	  	// 设置邮件内容
-      var mailOptions = {
-      from: "Fred Foo <13172615419@sina.cn>", // 发件地址
-      to: "13172615419@sina.cn", // 收件列表
-      subject: "订购成功", // 标题
-      html: "<b>thanks  for login!</b>" // html 内容
-         };	
-	  // 开启一个 SMTP 连接池
+app.post("/email",function(req,res){
+	console.log(req.body);
+// 开启一个 SMTP 连接池
 var smtpTransport = nodemailer.createTransport({
   host: "smtp.sina.com", // 主机
   secureConnection: true, // 使用 SSL
   port: 465, // SMTP 端口
   auth: {
     user: "13172615419@sina.cn", // 账号
-    pass: "goushi000" // 密码 
+    pass: "goushi000" // 密码
   }
 });
 
-   // 发送邮件
+// 设置邮件内容
+     var mailOptions = {
+  from: "Fred Foo <13172615419@sina.cn>", // 发件地址
+  to: "13172615419@sina.cn", // 收件列表
+  subject: "订购成功", // 标题
+  html: "<b>thanks  for login!</b>" // html 内容
+};
+
 smtpTransport.sendMail(mailOptions, function(error, response){
   if(error){
     console.log(error);
@@ -33,15 +41,19 @@ smtpTransport.sendMail(mailOptions, function(error, response){
     console.log("发送成功");
   }
   smtpTransport.close(); // 如果没用，关闭连接池
-});  
 });
 
-var server=http.createServer(function(req,res){   
-    res.writeHead(200,{'Content-Type':'text/javascript;charset=UTF-8',"Access-Control-Allow-Origin":"*"}); 
-    res.write("Hello NodeJs");  
-    res.end();  
+  res.send('Hello World!');
 });
 
-server.listen(8090,"localhost",function(){
+app.post("/login",function(req,res){
+	 console.log(req.body.username);
+   	if(req.body.username=='...'&&req.body.password=='...'){
+		res.json({success:1});
+	}
+});
+
+
+app.listen(8090,"localhost",function(){
          console.log("��ʼ����...");
 });
